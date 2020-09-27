b12af54ec0e616ea73dffaee68e1e407ce572ddd
tim24ktk <tim24ktk@mail.ru>
2020-09-27 22:23:03 +0300

    Правка замечаний 2

--- js/main.js
+++ js/main.js
@@ -1,53 +1,56 @@
 'use strict';
 
-//  функция создания массива из 8 сгенериванных JS объектов
-const ARRAYS_LENGTH = 8;
-const TYPES_OF_HOUSING = [`palace`, `flat`, `parking`, `house`, `elevator`, `bungalow`];
+const LENGTHS = 8;
+const TYPES_OF_HOUSING = [`palace`, `flat`, `house`, `bungalow`];
 const CHECKIN_CHECKOUT_TIMES = [`12:00`, `13:00`, `14:00`];
-const TYPES_OF_FEATURES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
+const TYPES = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 const PHOTOS = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
-const MAX_X = 1200;
-const MIN_Y = 130;
-const MAX_Y = 630;
+const MIN_X = 0; // мин x-координата точки с меткой
+const MAX_X = 1200; // мах x-координата точки с меткой
+const MIN_Y = 200; // мин у-координата точки с меткой
+const MAX_Y = 700; // макс y-координата точки с меткой
 const MAX_PRICE = 1000000;
 const MIN_ROOM = 1;
 const MAX_ROOM = 4;
 const MIN_GUEST = 1;
 const MAX_GUEST = 10;
+const PIN_OFFSET_X = 25;
+const PIN_OFFSET_Y = 70;
 
 // функция получения случайного числа
-const getRandomNumber = function (min, max) {
+const getRandomNumber = (min, max) => {
   return Math.random() * (max - min) + min;
 };
 
-const getRandomGeneration = function () {
+//  функция создания массива из 8 сгенериванных JS объектов
+const getRandomGeneration = () => {
   const temporaryArrays = [];
-  for (let i = 1; i <= ARRAYS_LENGTH; i++) {
+  for (let i = 1; i <= LENGTHS; i++) {
     const locations = {
-      x: getRandomNumber(0, MAX_X),
-      y: getRandomNumber(MIN_Y, MAX_Y)
+      x: Math.round(getRandomNumber(MIN_X, MAX_X)),
+      y: Math.round(getRandomNumber(MIN_Y, MAX_Y))
     };
 
     const declarationExample = {
       author: {
-        avatar: `img/avatars/user0` + i + `.png`
+        avatar: `img/avatars/user0${i}.png`
       },
       offer: {
-        title: `Заголовок предложения ` + i,
-        address: `` + locations.x + `, ` + locations.y + ``,
-        price: getRandomNumber(0, MAX_PRICE),
+        title: `Заголовок предложения ${i}`,
+        address: `${locations.x}, ${locations.y}`, // адрес на который указывает острый конец пина
+        price: Math.round(getRandomNumber(0, MAX_PRICE)),
         type: TYPES_OF_HOUSING[Math.round(getRandomNumber(0, TYPES_OF_HOUSING.length - 1))],
         rooms: Math.round(getRandomNumber(MIN_ROOM, MAX_ROOM)),
         guests: Math.round(getRandomNumber(MIN_GUEST, MAX_GUEST)),
-        checkin: `` + CHECKIN_CHECKOUT_TIMES[Math.round(getRandomNumber(0, CHECKIN_CHECKOUT_TIMES.length - 1))] + ``,
-        checkout: `` + CHECKIN_CHECKOUT_TIMES[Math.round(getRandomNumber(0, CHECKIN_CHECKOUT_TIMES.length - 1))] + ``,
-        features: TYPES_OF_FEATURES.slice(getRandomNumber(0, TYPES_OF_FEATURES.length - 1)),
-        description: `Описание ` + i,
-        photos: PHOTOS.slice(getRandomNumber(0, PHOTOS.length - 1))
+        checkin: `${CHECKIN_CHECKOUT_TIMES[Math.round(getRandomNumber(0, CHECKIN_CHECKOUT_TIMES.length - 1))]}`,
+        checkout: `${CHECKIN_CHECKOUT_TIMES[Math.round(getRandomNumber(0, CHECKIN_CHECKOUT_TIMES.length - 1))]}`,
+        features: TYPES.slice(Math.round(getRandomNumber(0, TYPES.length - 1))),
+        description: `Описание ${i}`,
+        photos: PHOTOS.slice(Math.round(getRandomNumber(0, PHOTOS.length - 1)))
       },
-      location: {
-        x: getRandomNumber(0, MAX_X),
-        y: getRandomNumber(MIN_Y, MAX_Y)
+      location: { // расположение пина
+        x: locations.x - PIN_OFFSET_X,
+        y: locations.y - PIN_OFFSET_Y
       }
     };
     temporaryArrays.push(declarationExample);
@@ -57,20 +60,17 @@ const getRandomGeneration = function () {
 const declarations = getRandomGeneration();
 
 //  убирает класс у map
-const map = document.querySelector('.map');
-map.classList.remove('map--faded');
+const map = document.querySelector(`.map`);
+map.classList.remove(`map--faded`);
 
 
 //  создание DOM элемента на основе JS-объекта
-const PIN_OFFSET_X = 25;
-const PIN_OFFSET_Y = 70;
-
-const createElementWithSimpleLabel = function (declaration) {
-  const pinContent = document.querySelector('#pin').content.querySelector('.map__pin');
+const pinContent = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+const createElementWithSimpleLabel = (declaration) => {
   const pinContentClone = pinContent.cloneNode(true);
-  const pinImg = pinContentClone.querySelector('img');
-  pinContentClone.style.left = declaration.location.x + PIN_OFFSET_X + 'px';
-  pinContentClone.style.top = declaration.location.y + PIN_OFFSET_Y + 'px';
+  const pinImg = pinContentClone.querySelector(`img`);
+  pinContentClone.style.left = `${declaration.location.x}px`;
+  pinContentClone.style.top = `${declaration.location.y}px`;
   pinImg.src = declaration.author.avatar;
   pinImg.alt = declaration.offer.title;
   return pinContentClone;
@@ -78,10 +78,10 @@ const createElementWithSimpleLabel = function (declaration) {
 
 
 // функция заполнения блока DOM-элементами на основе массива JS-объектов
-const createMapPins = function () {
-  const mapPins = map.querySelector('.map__pins');
+const mapPins = map.querySelector(`.map__pins`);
+const createMapPins = () => {
   const fragment = document.createDocumentFragment();
-  declarations.forEach(function fillingBlockWithElements(item) {
+  declarations.forEach((item) => {
     fragment.appendChild(createElementWithSimpleLabel(item));
   });
   mapPins.appendChild(fragment);
